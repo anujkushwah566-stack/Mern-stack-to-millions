7a3460231de702e4dbdbd90c88cc186099aff46a
ANuj <anujkushwah566@gmail.com>
2026-04-16 14:44:46 +0530

    chapter 47

---
+++ ch-47 welcome to react/first/src/App.jsx
@@ -0,0 +1,20 @@
+const App = ()=>{
+   
+  return(
+    <div style={{
+    width: 200,
+    height: 200,
+    backgroundColor: "red",
+    borderRadius: 20
+   }}>
+      <p style={{
+        fontSize: 100,
+        fontweight : "bold",
+        color: "white",
+        fontFamily: "sans-serif"
+      }}>hello sir</p>
+    </div>
+  )
+}
+
+export default App
